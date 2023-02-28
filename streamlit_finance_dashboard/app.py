1a16321519026b81a6c7a2ede885e330125c6121
kamath7 <adithyakamath96@gmail.com>
2023-03-01 03:34:12 +0530

    creating filters

--- streamlit_finance_dashboard/app.py
+++ streamlit_finance_dashboard/app.py
@@ -7,3 +7,17 @@ st.set_page_config(page_title="Sales View", page_icon=":bar_chart:",layout="wide
 
 df = pd.read_excel("./SampleData.xlsx","SalesOrders")
 st.dataframe(df)
+
+st.sidebar.header("Filters")
+
+region = st.sidebar.multiselect(
+    "Select the region",
+    options=df["Region"].unique(),
+    default=df["Region"].unique()
+)
+item = st.sidebar.multiselect(
+    "Select the Item",
+    options=df["Item"].unique(),
+    default=df["Item"].unique()
+)
+
