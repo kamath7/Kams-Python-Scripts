0610ed55c946b15fa265a36d2297e2de8348b09c
Adithya Kamath <adithyakamath96@gmail.com>
2024-06-14 00:33:41 +0530

    you can get matches from here

--- Euros_Script/app.py
+++ Euros_Script/app.py
@@ -11,12 +11,24 @@ Add timing also
 
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+import pytz
 
 
-url = "https://en.wikipedia.org/wiki/UEFA_Euro_2024"
+url = 'https://en.wikipedia.org/wiki/UEFA_Euro_2024'
+
 response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+html_content = response.text
+soup = BeautifulSoup(html_content, 'html.parser')
 
+tables = soup.find_all('table', class_='fevent')
 
-print(soup)
+matches = []
 
+for table in tables:
+    home_team = table.find('th', class_='fhome').find('span', itemprop='name').text.strip()
+    away_team = table.find('th', class_='faway').find('span', itemprop='name').text.strip()
+    matches.append(f"{home_team} v {away_team}")
+
+for match in matches:
+    print(match)
