6a86c46e063ee433fadafb141dbf8216bc79c4da
Adithya Kamath <adithyakamath96@gmail.com>
2024-06-14 00:55:39 +0530

    scraping eng and port matches

--- Euros_Script/app.py
+++ Euros_Script/app.py
@@ -13,6 +13,7 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 import pytz
+import pandas as pd 
 
 
 url = 'https://en.wikipedia.org/wiki/UEFA_Euro_2024'
@@ -23,12 +24,29 @@ soup = BeautifulSoup(html_content, 'html.parser')
 
 tables = soup.find_all('table', class_='fevent')
 
-matches = []
 
+matches_portugal = []
+matches_england = []
+
+# Iterate through each table of fixtures
 for table in tables:
     home_team = table.find('th', class_='fhome').find('span', itemprop='name').text.strip()
     away_team = table.find('th', class_='faway').find('span', itemprop='name').text.strip()
-    matches.append(f"{home_team} v {away_team}")
+    match_details = f"{home_team} v {away_team}"
+    
+    # Check if the match involves Portugal
+    if 'Portugal' in match_details:
+        matches_portugal.append({
+            'Match': match_details
+        })
+    
+    # Check if the match involves England
+    if 'England' in match_details:
+        matches_england.append({
+            'Match': match_details
+        })
 
-for match in matches:
-    print(match)
+
+df_portugal_matches = pd.DataFrame(matches_portugal)
+
+df_england_matches = pd.DataFrame(matches_england)
