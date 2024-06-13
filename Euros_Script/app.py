5a4f682c86f36b131133cc332b62888aebb86205
Adithya Kamath <adithyakamath96@gmail.com>
2024-06-13 23:50:35 +0530

    initial code

--- Euros_Script/app.py
+++ Euros_Script/app.py
@@ -8,3 +8,15 @@ Add timing also
 
 
 '''
+
+import requests
+from bs4 import BeautifulSoup
+
+
+url = "https://en.wikipedia.org/wiki/UEFA_Euro_2024"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+
+print(soup)
+
